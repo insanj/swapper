c4c929934e690036881de05fb10bfb2ce2f428ba
insanj <me@insanj.com>
2019-03-15 02:38:36 -0500

    workflow of both file reader, nbt parsing flows, input filereader

--- index.js
+++ index.js
@@ -1,116 +1,76 @@
 
-function tigonYAMLFormSubmit() {
-    const yamlString = document.getElementById("tigon-form-input").value;
-    const yamlJSON = YAML.parse(yamlString);
-    let convertedJSON = {};
-    for (let toplevelKey in yamlJSON) {
-        if (toplevelKey !== "worlds") {
-            convertedJSON[toplevelKey] = yamlJSON[toplevelKey];
-        } else {
-            let worlds = yamlJSON[toplevelKey];
-            let parsedWorlds = {};
-            for (let worldUIDString in worlds) {
-                let areas = worlds[worldUIDString];
-                let parsedAreas = {};
-                for (let areaName in areas) {
-                    let areaLoctionString = areas[areaName];
-                    const splitCoords = areaLoctionString.split(',');
-                    parsedAreas[areaName] = {
-                        "x": parseFloat(splitCoords[0]),
-                        "y": parseFloat(splitCoords[1]),
-                        "z": parseFloat(splitCoords[2])
-                    };
-                }
-                parsedWorlds[worldUIDString] = parsedAreas;
-            }
-            convertedJSON[toplevelKey] = parsedWorlds;
-        }
-    }
+function swapper_singleplayerToMultiplayer(playerUUID, levelDatFile) {
+  const reader = new FileReader();
+  reader.onerror = error => reject(error);
+  reader.onload = event => {
+     // take level.dat, Player entry and fill in a new compound tag
+     const compressedNBTData = event.target.result;
+     const uncompressedNBTData = pako.inflate(compressedNBTData);
+     nbt.parse(uncompressedNBTData, function(error, data) {
+         // grab the Player tag from the level dat (nested inside level.dat > Data > Player)
+         const levelDatPlayerTag = data.value['Data'].value['Player'];
 
-    console.log("Parsing back into YAML..." + JSON.stringify(convertedJSON, null, '\t'));
-    const yamlConvertedJSON = YAML.stringify(convertedJSON, 20, 2).replace("\n","<br>");
-    document.getElementById("tigon-result").innerHTML = yamlConvertedJSON;
-}
+         // write new compound tag to the final file buffer
+         const nbtArrayBuffer = nbt.writeUncompressed(levelDatPlayerTag);
 
-function tigonJSONFormSubmit() {
-    const yamlString = document.getElementById("tigon-form-input").value;
-    const yamlJSON = YAML.parse(yamlString);
-    console.log("Dumping JSON instead of YAML..." + JSON.stringify(yamlJSON, null, '\t'));
-    document.getElementById("tigon-result").innerHTML = JSON.stringify(yamlJSON);
-}
-
-function tigonNBTFormSubmit() {
-  const yamlString = document.getElementById("tigon-form-input").value;
-  const yamlJSON = YAML.parse(yamlString);
-
-  let nbtWorldsCompoundTag = {};
-  for (let worldUUID in yamlJSON["worlds"]) {
-    const worldAreas = yamlJSON["worlds"][worldUUID];
-    let worldCompoundTag = {};
-    for (let areaName in worldAreas) {
-      const area = worldAreas[areaName];
-      worldCompoundTag[areaName] = {
-        type: 'compound', value: {
-          x: { type: 'double', value: area["x"] },
-          y: { type: 'double', value: area["y"] },
-          z: { type: 'double', value: area["z"] }
-        }
-      };
-    }
-
-    nbtWorldsCompoundTag[worldUUID] = {
-      type: 'compound', value: worldCompoundTag
-    };
+         // download file
+         const nbtBytesArray = new Uint8Array(nbtArrayBuffer);
+         var blob = new Blob([nbtBytesArray], {type: "application/nbt"});
+         var link = document.createElement('a');
+         link.href = window.URL.createObjectURL(blob);
+         var fileName = playerUUID + ".dat";
+         link.download = fileName;
+         link.click();
+     });
   }
-
-  const nbtRootCompoundTag = {
-      name: 'ViridianPersistentState',
-      value: {
-        DataVersion: {
-          type: 'int', value: 1935
-        },
-        data: {
-          type: 'compound', value: {
-            pride_worlds: {
-              type: 'compound', value: nbtWorldsCompoundTag
-            }
-          }
-        }
-      }
-  };
-
-  console.log("Translated YAML to NBT root compound tag: " + JSON.stringify(nbtRootCompoundTag));
-  const nbtArrayBuffer = nbt.writeUncompressed(nbtRootCompoundTag);
-
-  const nbtBytesArray = new Uint8Array(nbtArrayBuffer);
-  var blob = new Blob([nbtBytesArray], {type: "application/nbt"});
-  var link = document.createElement('a');
-  link.href = window.URL.createObjectURL(blob);
-  var fileName = 'ViridianPersistentState.dat';
-  link.download = fileName;
-  link.click();
-
-//  document.getElementById("tigon-iframe").src = yamlConvertedJSON;
+  reader.readAsArrayBuffer(levelDatFile);
 }
 
-document.getElementById("tigon-form-submit").onclick = function(event) {
+function swapper_multiplayerToSingleplayer(uuidDatFile, levelDatFile) {
+  // parse both UUID.dat and level.dat
+  nbt.parse(uuidDatFile, function(error, uuidData) {
+    nbt.parse(levelDatFile, function(error, levelData) {
+      // grab the root level tag from the uuid file
+      const uuidPlayerTag = uuidData.value;
+
+      // write this tag as "Player" in the level.dat file, nested under leveldat > Data > Player
+      levelData.value['Data'].value['Player'].value = uuidPlayerTag;
+
+      // write new compound tag to the final file buffer
+      const nbtArrayBuffer = nbt.writeUncompressed(levelData);
+
+      // download file
+      const nbtBytesArray = new Uint8Array(nbtArrayBuffer);
+      var blob = new Blob([nbtBytesArray], {type: "application/nbt"});
+      var link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      var fileName = levelDatFile + ".dat";
+      link.download = fileName;
+      link.click();
+    });
+  });
+}
+
+document.getElementById("single-to-multi-submit").onclick = function(event) {
     event.preventDefault();
 
-    if (window.location.href.indexOf('json') >= 0) {
-      tigonJSONFormSubmit();
-    } else if (window.location.href.indexOf('yaml') >= 0) {
-      tigonYAMLFormSubmit();
-    } else {
-      tigonNBTFormSubmit();
+    try {
+      const playerUUID = document.getElementById("single-to-multi-text-input").value;
+      const levelDatFile = document.getElementById("single-to-multi-file").files[0];
+      swapper_singleplayerToMultiplayer(playerUUID, levelDatFile);
+    } catch (e) {
+      alert(e);
     }
 };
 
-document.addEventListener('DOMContentLoaded', function(){
-  if (window.location.href.indexOf('json') >= 0) {
-    document.getElementById("tigon-form-input").setAttribute("placeholder", "YAML -> JSON");
-  } else if (window.location.href.indexOf('yaml') >= 0) {
-    document.getElementById("tigon-form-input").setAttribute("placeholder", "0.3.x YAML -> 0.4.x YAML");
-  } else {
-    document.getElementById("tigon-form-input").setAttribute("placeholder", "YAML -> NBT");
-  }
-}, false);
+document.getElementById("multi-to-single-submit").onclick = function(event) {
+    event.preventDefault();
+
+    try {
+      const uuidFile = document.getElementById("multi-to-single-uuid-file").files[0];
+      const levelDatFile = document.getElementById("multi-to-single-level-file").files[0];
+      swapper_multiplayerToSingleplayer(uuidFile, levelDatFile);
+    } catch (e) {
+      alert(e);
+    }
+};
