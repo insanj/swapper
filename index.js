61ef86edb974d0fb033c81b5d97852a139cba3fc
insanj <me@insanj.com>
2019-03-15 18:05:46 -0500

    async/await-ify multiplayer solution, get running!

--- index.js
+++ index.js
@@ -28,28 +28,59 @@ function swapper_singleplayerToMultiplayer(playerUUID, levelDatFile) {
 }
 
 function swapper_multiplayerToSingleplayer(uuidDatFile, levelDatFile) {
-  // parse both UUID.dat and level.dat
-  nbt.parse(uuidDatFile, function(error, uuidData) {
-    nbt.parse(levelDatFile, function(error, levelData) {
-      // grab the root level tag from the uuid file
-      const uuidPlayerTag = uuidData.value;
+  const readFile = async function(file) {
+      return await new Promise((resolve, reject) => {
+        const reader = new FileReader();
+        reader.onerror = error =>{
+          reject(error);
+        }
+        reader.onload = event => {
+           resolve(event.target.result);
+        }
+        reader.readAsArrayBuffer(file);
+      });
+  }
 
-      // write this tag as "Player" in the level.dat file, nested under leveldat > Data > Player
-      levelData.value['Data'].value['Player'].value = uuidPlayerTag;
+  const parseNBTData = async function(unparsedData) {
+      return await new Promise((resolve, reject) => {
+        nbt.parse(unparsedData, function(error, data) {
+          if (error) { reject(error); }
+          else { resolve(data); }
+        });
+      });
+  }
 
-      // write new compound tag to the final file buffer
-      const nbtArrayBuffer = nbt.writeUncompressed(levelData);
+  const asyncWorkflow = async function(uuidFle, levelFile) {
+    // take level.dat, Player entry and fill in a new compound tag
+    const compressedUUIDDatFile = await readFile(uuidFle);
+    const compressedLevelDatFile = await readFile(levelFile);
+    const uncompressedUUIDDatFile = pako.inflate(compressedUUIDDatFile);
+    const uncompressedLevelDatFile = pako.inflate(compressedLevelDatFile);
 
-      // download file
-      const nbtBytesArray = new Uint8Array(nbtArrayBuffer);
-      var blob = new Blob([nbtBytesArray], {type: "application/nbt"});
-      var link = document.createElement('a');
-      link.href = window.URL.createObjectURL(blob);
-      var fileName = levelDatFile + ".dat";
-      link.download = fileName;
-      link.click();
-    });
-  });
+    // parse both UUID.dat and level.dat
+    const parsedUUIDDatData = await parseNBTData(uncompressedUUIDDatFile);
+    const parsedLevelDatData = await parseNBTData(uncompressedLevelDatFile);
+
+    // grab the root level tag from the uuid file
+    const uuidPlayerTag = parsedUUIDDatData.value; // might need name ref
+
+    // write this tag as "Player" in the level.dat file, nested under leveldat > Data > Player
+    parsedLevelDatData.value['Data'].value['Player'].value = uuidPlayerTag;
+
+    // write new compound tag to the final file buffer
+    const nbtArrayBuffer = nbt.writeUncompressed(parsedLevelDatData);
+
+    // download file
+    const nbtBytesArray = new Uint8Array(nbtArrayBuffer);
+    var blob = new Blob([nbtBytesArray], {type: "application/nbt"});
+    var link = document.createElement('a');
+    link.href = window.URL.createObjectURL(blob);
+    var fileName ="level.dat";
+    link.download = fileName;
+    link.click();
+  }
+
+  asyncWorkflow(uuidDatFile, levelDatFile).then().catch(e => { console.error(e); });
 }
 
 document.getElementById("single-to-multi-submit").onclick = function(event) {
